dbb1c10e998095412ef829e6498940a87e967251
Matt Fellenz <mattf53190@gmail.com>
2021-08-27 15:01:51 -0700

    correct DB.sql
    Username and password are non-null, and username is unique.

--- server/DB.sql
+++ server/DB.sql
@@ -1,6 +1,6 @@
 create table users (
 	id bigint unsigned not null auto_increment primary key,
-	username varchar(20),
-	password char(64) binary, -- bcrypt hash
+	username varchar(20) not null unique,
+	password char(64) binary not null, -- bcrypt hash
 	display_name varchar(40)
 )
