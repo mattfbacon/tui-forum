3fe8a00ad7290df185946b9a938567300cecc5ad
Matt Fellenz <mattf53190@gmail.com>
2021-09-12 12:53:32 -0700

    Forgot DELETE /users/session

--- server/src/Routes/users/session.cpp
+++ server/src/Routes/users/session.cpp
@@ -16,9 +16,14 @@ ROUTE_IMPL_NOEXCEPT(post, res, req) {
 	(void)req;
 #warning "TODO"
 }
+ROUTE_IMPL_NOEXCEPT(delete_, res, req) {
+	(void)res;
+	(void)req;
+#warning "TODO"
+}
 
 ROUTES_REGISTERER_IMPL(app) {
-	return app.any(path, HTTP::send_code_handler<HTTP::Status::METHOD_NOT_ALLOWED>).get(path, get).post(path, post);
+	return app.any(path, HTTP::send_code_handler<HTTP::Status::METHOD_NOT_ALLOWED>).get(path, get).post(path, post).del(path, delete_);
 }
 }  // namespace session
 }  // namespace users
