64786a0f1ebc4f94a2df42cc92c4ceacc472184b
Matt Fellenz <mattf53190@gmail.com>
2021-09-15 10:18:50 -0700

    PostgreSQL syntax for DB.sql

--- server/DB.sql
+++ server/DB.sql
@@ -1,6 +1,6 @@
 create table users (
-	id bigint unsigned not null auto_increment primary key,
+	id bigserial primary key,
 	username varchar(20) not null unique,
-	password char(60) binary not null, -- bcrypt hash
+	password bytea not null, -- bcrypt hash
 	display_name varchar(40)
 )
