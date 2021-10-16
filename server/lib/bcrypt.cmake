900162a0d44aeee6f805236a6cf80e2864740c0e
Matt Fellenz <mattf53190@gmail.com>
2021-10-16 15:37:06 -0700

    Convert from OBJECT sub-libraries to STATIC

--- server/lib/bcrypt.cmake
+++ server/lib/bcrypt.cmake
@@ -1,5 +1,5 @@
 set(bcrypt_sources bcrypt.c crypt_blowfish.c crypt_gensalt.c wrapper.c x86.S)
 list(TRANSFORM bcrypt_sources PREPEND ${CMAKE_CURRENT_LIST_DIR}/bcrypt/src/)
-add_library(bcrypt OBJECT ${bcrypt_sources})
+add_library(bcrypt STATIC ${bcrypt_sources})
 target_include_directories(bcrypt PRIVATE ${CMAKE_CURRENT_LIST_DIR}/bcrypt/include/bcrypt)
 target_include_directories(bcrypt SYSTEM INTERFACE ${CMAKE_CURRENT_LIST_DIR}/bcrypt/include)
--- server/lib/taopq.cmake
+++ server/lib/taopq.cmake
@@ -1,6 +1,6 @@
 file(GLOB_RECURSE taopq_sources ${CMAKE_CURRENT_LIST_DIR}/taopq/src/lib/pq/*.cpp)
 
-add_library(taopq OBJECT ${taopq_sources})
+add_library(taopq STATIC ${taopq_sources})
 target_include_directories(taopq PRIVATE ${CMAKE_CURRENT_LIST_DIR}/taopq/include)
 target_include_directories(taopq SYSTEM INTERFACE ${CMAKE_CURRENT_LIST_DIR}/taopq/include)
 target_link_libraries(taopq PUBLIC pq)
