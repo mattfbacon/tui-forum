d802ef916fd0a14d9037b97cd3a78a2c65f0b5dd
Matt Fellenz <mattf53190@gmail.com>
2021-11-22 20:22:32 -0800

    Test client: squash error if missing content

--- test-client/index.js
+++ test-client/index.js
@@ -32,7 +32,8 @@ const repl = () => {
 		const split = cmd.split(' ');
 		const method = split[0].toUpperCase();
 		const url = decodeURIComponent(split[1]);
-		const content = eval(`(${cmd.slice(method.length + 1 + split[1].length + 1)})`);
+		const raw_content = cmd.slice(method.length + 1 + split[1].length + 1);
+		const content = raw_content.length > 0 ? eval(`(${raw_content})`) : undefined;
 		let fetch_response;
 		try {
 			fetch_response = await fetch('http://localhost:9000' + url, { method, ...(method == 'GET' || method == 'HEAD' ? {} : { body: encode(content) }) });
